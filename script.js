3093d470f38dc7c34c23f164c4c50b8f38fc1483
yapeansa <yago.pereira@estudante.ufjf.br>
2023-10-21 04:02:39 -0300

    Adicionando áudio.

--- script.js
+++ script.js
@@ -2,9 +2,43 @@ const html = document.querySelector('html')
 const botoes = document.querySelectorAll('.app__card-button')
 const banner = document.querySelector('.app__image')
 const titulo = document.querySelector('.app__title')
+const startPauseBt = document.getElementById('start-pause')
 const musicaFocoInput = document.getElementById('alternar-musica')
 const musica = new Audio('./sons/luna-rise-part-one.mp3')
+const iniciarPlay = new Audio('./sons/play.wav')
+const iniciarPause = new Audio('./sons/pause.mp3')
+const beep = new Audio('./sons/beep.mp3')
+let tempoDecorridoEmSegundos = 5
+let intervaloId = null
+
 musica.loop = true
+
+const contagemRegressiva = () => {
+    console.log(tempoDecorridoEmSegundos)
+    if (tempoDecorridoEmSegundos <= 0) {
+        zerar()
+        beep.paused ? beep.play() : beep.pause()
+        return
+    }
+    tempoDecorridoEmSegundos -= 1
+}
+
+function iniciarOuPausar() {
+    (iniciarPlay.paused && !intervaloId) ? iniciarPlay.play() : iniciarPlay.pause()
+    if (intervaloId) {
+        iniciarPause.paused? iniciarPause.play() : iniciarPause.pause()
+        zerar()
+        return
+    }
+    intervaloId = setInterval(contagemRegressiva, 1000)
+}
+
+function zerar() {
+    clearInterval(intervaloId)
+    intervaloId = null
+}
+
+startPauseBt.addEventListener('click', iniciarOuPausar)
 
 musicaFocoInput.addEventListener('change', () => {
     if (musica.paused) {
