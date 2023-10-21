3752bded6c4e5892fac2ed63377e28ad4b28ef94
yapeansa <yago.pereira@estudante.ufjf.br>
2023-10-21 06:17:20 -0300

    Conclusão do projeto Fokus

--- script.js
+++ script.js
@@ -4,33 +4,60 @@ const banner = document.querySelector('.app__image')
 const titulo = document.querySelector('.app__title')
 const startPauseBt = document.getElementById('start-pause')
 const musicaFocoInput = document.getElementById('alternar-musica')
+const tempoNaTela = document.querySelector('#timer')
+
 const musica = new Audio('./sons/luna-rise-part-one.mp3')
 const iniciarPlay = new Audio('./sons/play.wav')
 const iniciarPause = new Audio('./sons/pause.mp3')
 const beep = new Audio('./sons/beep.mp3')
-let tempoDecorridoEmSegundos = 5
+let tempoDecorridoEmSegundos = 1500
 let intervaloId = null
 
 musica.loop = true
 
-const contagemRegressiva = () => {
-    console.log(tempoDecorridoEmSegundos)
+function mostrarTempo() {
+    const tempo = new Date(tempoDecorridoEmSegundos * 1000)
+    const tempoFormatado = tempo.toLocaleString('pt-br', { minute: '2-digit', second: '2-digit' })
+    tempoNaTela.innerHTML = `${tempoFormatado}`
+}
+
+function contagemRegressiva() {
+    //console.log(tempoDecorridoEmSegundos)
     if (tempoDecorridoEmSegundos <= 0) {
         zerar()
         beep.paused ? beep.play() : beep.pause()
+        startPauseBt.childNodes[3].textContent = "Começar"
+        startPauseBt.childNodes[1].setAttribute('src', './imagens/play_arrow.png')
+        const atributoDocumento = html.getAttribute('data-contexto')
+        switch (atributoDocumento) {
+            case 'descanso-longo':
+                tempoDecorridoEmSegundos = 900
+                break
+            case 'descanso-curto':
+                tempoDecorridoEmSegundos = 300
+                break
+            default:
+                tempoDecorridoEmSegundos = 1500
+                break
+        }
         return
     }
     tempoDecorridoEmSegundos -= 1
+    mostrarTempo()
 }
 
 function iniciarOuPausar() {
     (iniciarPlay.paused && !intervaloId) ? iniciarPlay.play() : iniciarPlay.pause()
     if (intervaloId) {
-        iniciarPause.paused? iniciarPause.play() : iniciarPause.pause()
+        iniciarPause.paused ? iniciarPause.play() : iniciarPause.pause()
+        startPauseBt.childNodes[3].textContent = "Começar"
+        startPauseBt.childNodes[1].setAttribute('src', './imagens/play_arrow.png')
         zerar()
         return
     }
     intervaloId = setInterval(contagemRegressiva, 1000)
+    startPauseBt.childNodes[3].textContent = "Pausar"
+    startPauseBt.childNodes[1].setAttribute('src', './imagens/pause.png')
 }
 
 function zerar() {
@@ -48,10 +75,9 @@ musicaFocoInput.addEventListener('change', () => {
     }
 })
 
-const alterarContexto = (contexto, botao) => {
+function alterarContexto(contexto, botao, tempoId) {
     html.setAttribute('data-contexto', `${contexto}`)
     banner.setAttribute('src', `./imagens/${contexto}.png`)
-
     switch (contexto) {
         case 'descanso-curto':
             titulo.innerHTML = `
@@ -74,10 +100,8 @@ Otimize sua produtividade,<br>
     }
 
     !botao.classList.contains('active') ? botao.classList.add('active') : ''
-}
-
-const focoBotao = () => {
-
+    tempoDecorridoEmSegundos = tempoId
+    mostrarTempo()
 }
 
 botoes.forEach(botao => {
@@ -85,11 +109,13 @@ botoes.forEach(botao => {
         botoes.forEach(botao => botao.classList.remove('active'))
         const atributo = botao.getAttribute('data-contexto')
         if (atributo === 'short') {
-            alterarContexto('descanso-curto', botao)
+            alterarContexto('descanso-curto', botao, 300)
         } else if (atributo === 'long') {
-            alterarContexto('descanso-longo', botao)
+            alterarContexto('descanso-longo', botao, 900)
         } else {
-            alterarContexto('foco', botao)
+            alterarContexto('foco', botao, 1500)
         }
     })
 })
+
+mostrarTempo()
